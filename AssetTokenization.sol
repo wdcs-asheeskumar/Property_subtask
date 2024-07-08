e6fc74420439ee12e73e3b5620be3ee790f5b461
wdcs-asheeskumar <154244737+wdcs-asheeskumar@users.noreply.github.com>
2024-07-08 19:21:51 +0530

    Update AssetTokenization.sol

--- AssetTokenization.sol
+++ AssetTokenization.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "/home/webclues/Desktop/MyProjects/Subtask/subtask/src/UsdtMock.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 /// @dev MockUsdt contract
-
 contract UsdtMock is ERC20 {
     constructor() ERC20("MockUsdt", "USDT") {
         // _mint(msg.sender, 10000000000 * (10 ** decimals()));
     }
 
-    function mintToken(address to, uint256 value) public {
-        _mint(to, value);
+    /// @dev function to mint token and transfer it from investor to owner
+    function mintToken(address investor, address owner, uint256 value) public {
+        _mint(investor, value);
+        _transfer(investor, owner, value);
     }
-    function giveApproval(address _address, uint256 value) public {
-        approve(_address, value);
-    }
+    // function giveApproval(address _address, uint256 value) public {
+    //     approve(_address, value);
+    // }
 
     function checkApproval(
         address owner,
@@ -26,38 +26,57 @@ contract UsdtMock is ERC20 {
         return (allowance(owner, spender));
     }
 
-    function transferFunds(address to, uint256 value) public {
-        transfer(to, value);
+    /// @dev function to transfer funds from investor to owner as well as from owner to investor
+    function transferFunds(
+        address investor,
+        address owner,
+        uint256 value
+    ) public {
+        _transfer(investor, owner, value);
     }
 
+    /// @dev function to check the USDT balance of the user.
     function checkBalance(address owner) public view returns (uint256) {
         return balanceOf(owner);
     }
 }
 
+/// @dev NFT contract to mint NFT in the owner's account when he lists his property
+contract PropertyNFT is ERC721 {
+    constructor() ERC721("PropertyNFT", "PNFT") {}
+
+    function safeMint(address to, uint256 tokenId) public {
+        _safeMint(to, tokenId);
+    }
+}
+
+/// @dev Asset tokenization contract
+
 contract AssetTokenization is ERC20 {
+    /// @dev struct to store the data of property.
     struct PropertyOwnerDetails {
-        uint256 propertyId;
+        uint256 propertyId; // 1
         address propertyOwnerAddress;
-        uint256 valueOfProperty;
-        uint256 valueAvailableForInvestment;
-        uint256 annualEarning;
+        uint256 valueOfProperty; // 50000
+        uint256 valueAvailableForInvestment; // 20000
+        uint256 annualEarning; // 24000
         uint256 monthlyEarningFromInvestment;
-        uint256 valueInvested;
-        uint256 valueCurrentlyAvailable;
+        uint256 valueInvested; /// @dev Amount of total investment done by all investors till now.
+        uint256 valueCurrentlyAvailable; /// @dev Amount of value of property currently remaining.
         uint256 noOfInvestors;
     }
 
     /// @dev keeping tract of all the property and there details.
     mapping(uint256 => PropertyOwnerDetails) propertyDetailsMapping;
 
-    /// @dev keeping the list of owners of the property
-    mapping(address => uint256) ownerPropertyList;
+    /// @dev keeping the list of number of owners of the property
+    mapping(address => uint256) public ownerPropertyList;
 
     /// @dev keeping the record of listed properties
-    mapping(uint256 => bool) listedProperties;
+    mapping(uint256 => bool) public listedProperties;
+
     /// @dev keeps record of the investor has invested in certain property or not.
-    mapping(uint256 => mapping(address => bool)) investmentRecords;
+    mapping(uint256 => mapping(address => bool)) public investmentRecords;
 
     /// @dev keeps record of the amount of investment done by the investor in a particular property.
     mapping(uint256 => mapping(address => uint256)) investmentAmountRecords;
@@ -69,6 +88,7 @@ contract AssetTokenization is ERC20 {
     uint256 private timeFrame = 30 days;
     uint256 public id = 0;
 
+    /// @dev emits when the property is listed
     event ListPropertyInfo(
         uint256 _id,
         address _ownerAddress,
@@ -77,6 +97,7 @@ contract AssetTokenization is ERC20 {
         uint256 _annualEarning
     );
 
+    /// @dev emits when an investor invests in some property.
     event InvestmentRecord(
         uint256 _propertyId,
         uint256 _amountToInvest,
@@ -86,13 +107,17 @@ contract AssetTokenization is ERC20 {
 
     // ERC20 private USDT_Address =
     //     ERC20(0x04c1A796D9049ce70c2B4A188Ae441c4c619983c);
-    address InvestorsUSDTToken;
 
     UsdtMock public usdtMock;
 
-    // UsdtMock public usdtMock;
-    constructor(address _usdtMock) ERC20("MyPropertyToken", "MPK") {
+    PropertyNFT public propertyNFT;
+
+    constructor(
+        address _usdtMock,
+        address _propertyNFT
+    ) ERC20("MyPropertyToken", "MPK") {
         usdtMock = UsdtMock(_usdtMock);
+        propertyNFT = PropertyNFT(_propertyNFT);
     }
 
     /// @dev function for the owner to list his property.
@@ -101,14 +126,16 @@ contract AssetTokenization is ERC20 {
         uint256 _valueOfProperty,
         uint256 _valueAvailableForInvestment,
         uint256 _annualEarning
-    ) public returns (bool) {
+    ) public {
         require(_ownerAddress != address(0), "Invalid address");
         require(_valueOfProperty > 0, "Invalid value of property");
         require(
-            _valueAvailableForInvestment < _valueOfProperty &&
+            _valueAvailableForInvestment <= _valueOfProperty &&
                 _valueAvailableForInvestment > 0,
             "Invalid value available for investment"
         );
+        require(listedProperties[id] == false, "Property allready listed");
+
         id = id + 1;
 
         uint256 monthlyIncomeFromInestment = (_valueAvailableForInvestment *
@@ -127,8 +154,10 @@ contract AssetTokenization is ERC20 {
         ownerPropertyList[_ownerAddress] = ownerPropertyList[_ownerAddress] + 1;
 
         listedProperties[id] = true;
-        _mint(_ownerAddress, _valueOfProperty);
+        _mint(_ownerAddress, _valueOfProperty); /// @dev minting tokens equivalent to the value of property in owner's account.
+        propertyNFT.safeMint(_ownerAddress, 1); /// @dev minting NFT token in the user's account.
 
+        /// @dev emiting the event ListProppertyInfo for emitting the information about the property.
         emit ListPropertyInfo(
             id,
             _ownerAddress,
@@ -137,7 +166,7 @@ contract AssetTokenization is ERC20 {
             _annualEarning
         );
 
-        return listedProperties[id];
+        // return listedProperties[id];
     }
 
     /// @dev function to check the current status of the property.
@@ -172,7 +201,6 @@ contract AssetTokenization is ERC20 {
             listedProperties[_propertyId] == true,
             "Property not listed or has been de-listed"
         );
-
         require(
             investmentRecords[_propertyId][_investorsAddress] == false,
             "Investor has already invested in this property"
@@ -207,52 +235,72 @@ contract AssetTokenization is ERC20 {
             _investorsAddress
         ] = _amountToInvest;
 
-        // ERC20(usdtMock).mint(
-        //     msg.sender,
-        //     propertyDetailsMapping[_propertyId].propertyOwnerAddress,
-        //     _amountToInvest
-        // );
-
-        usdtMock.mintToken(_investorsAddress, _amountToInvest);
-        ERC20(usdtMock).transferFrom(
+        usdtMock.mintToken(
             _investorsAddress,
             propertyDetailsMapping[_propertyId].propertyOwnerAddress,
             _amountToInvest
         );
 
+        _approve(
+            propertyDetailsMapping[_propertyId].propertyOwnerAddress,
+            _investorsAddress,
+            _amountToInvest
+        );
         transferFrom(
             propertyDetailsMapping[_propertyId].propertyOwnerAddress,
             _investorsAddress,
             _amountToInvest
         );
+
+        /// @dev emiting the InvestmentRecord event to give the information about the Investment.
         emit InvestmentRecord(
             _propertyId,
             _amountToInvest,
             block.timestamp,
             investmentRecords[_propertyId][_investorsAddress]
         );
-        // return toCheckUsdt(_investorsAddress);
+
+        // return balanceOf(_investorsAddress);
     }
 
     /// @dev function for allocating monthly rent to the investors.
-    function monthlyRent(uint256 _propertyId, address _investorAddress) public {
+    function monthlyRent(
+        uint256 _propertyId,
+        address _investorAddress
+    ) public returns (uint256) {
         require(
             listedProperties[_propertyId] == true,
             "Property not listed or has been de-listed"
         );
 
         require(
-            investmentTimeRecord[_propertyId][_investorAddress] + timeFrame >=
-                block.timestamp,
+            block.timestamp >=
+                investmentTimeRecord[_propertyId][_investorAddress] + timeFrame,
             "Rent can't be approved before 30 days"
         );
+
+        /// @dev fetching the total investment done by the investor in that particular property.
         uint256 _investment = investmentAmountRecords[_propertyId][
             _investorAddress
         ];
+
+        /// @dev calculating the monthly rent to be rewarded to the investor in that particular property.
         uint256 _rentValue = (_investment *
             propertyDetailsMapping[_propertyId].monthlyEarningFromInvestment) /
             propertyDetailsMapping[_propertyId].valueAvailableForInvestment;
-        transfer(_investorAddress, _rentValue);
+        // uint256 _rentValue = 100;
+        // _approve(owner, spender, value);
+        _approve(
+            propertyDetailsMapping[_propertyId].propertyOwnerAddress,
+            _investorAddress,
+            _rentValue
+        );
+        transferFrom(
+            propertyDetailsMapping[_propertyId].propertyOwnerAddress,
+            _investorAddress,
+            _rentValue
+        );
+        return _rentValue;
     }
 
     /// @dev function for withdrawing all investments by the investor
@@ -268,25 +316,45 @@ contract AssetTokenization is ERC20 {
             investmentRecords[_propertyId][_investorAddress] == true,
             "Only the owner of the property can withdraw the funds"
         );
-        uint256 _transaferAmount = investmentAmountRecords[_propertyId][
+        uint256 _transferAmount = investmentAmountRecords[_propertyId][
             _investorAddress
         ];
-        transferFrom(
+
+        /// @dev returning the USDT token from the owner's account to the investor.
+        usdtMock.transferFunds(
             propertyDetailsMapping[_propertyId].propertyOwnerAddress,
             _investorAddress,
-            _transaferAmount
+            _transferAmount
         );
     }
 
-    function deListingProperty(uint256 _propertyId) public view returns (bool) {
-        require(
-            listedProperties[_propertyId] == true,
-            "Property has already been delisted"
-        );
+    /// @dev function for getting the state of properties, wether they are listed or not.
+    function listedPropertyState(
+        uint256 _propertyId
+    ) public view returns (bool) {
+        // require(
+        //     listedProperties[_propertyId] == true,
+        //     "Property has already been delisted or has never been listed"
+        // );
         listedProperties[_propertyId] == false;
         return listedProperties[_propertyId];
     }
 
+    /// @dev function for delisting the properties
+    function deListingProperty(uint256 _id) public {
+        require(
+            listedProperties[_id] == true,
+            "Property has already been delisted or has never been listed"
+        );
+        require(
+            msg.sender == propertyDetailsMapping[_id].propertyOwnerAddress,
+            "Only owner can delist his/her property"
+        );
+
+        listedProperties[_id] = false;
+    }
+
+    /// @dev function for checking value of the property
     function checkValueOfProperty(
         uint256 _propertyId
     ) public view returns (uint256) {
@@ -297,36 +365,51 @@ contract AssetTokenization is ERC20 {
         return (propertyDetailsMapping[_propertyId].valueOfProperty);
     }
 
+    /// @dev function for check the value of property available for investment.
     function checkValueAvailableForInvestment(
         uint256 _propertyId
     ) public view returns (uint256) {
+        require(
+            listedProperties[_propertyId] == true,
+            "Property has not been listed"
+        );
         return (
             propertyDetailsMapping[_propertyId].valueAvailableForInvestment
         );
     }
 
+    /// @dev function for checking the current available value of property for investment.
     function checkvalueCurrentlyAvailable(
         uint256 _propertyId
     ) public view returns (uint256) {
+        require(
+            listedProperties[_propertyId] == true,
+            "Property has not been listed"
+        );
         return (propertyDetailsMapping[_propertyId].valueCurrentlyAvailable);
     }
 
+    /// @dev function to check annual earning from the property.
     function checkannualEarning(
         uint256 _propertyId
     ) public view returns (uint256) {
+        require(
+            listedProperties[_propertyId] == true,
+            "Property has not been listed"
+        );
         return (propertyDetailsMapping[_propertyId].annualEarning);
     }
 
-    function checkBalance(address _ownerAddress) public view returns (uint256) {
-        return balanceOf(_ownerAddress);
+    /// @dev function for checking the balance.
+    function checkBalance(address _user) public view returns (uint256) {
+        require(_user != address(0), "Invalid address");
+        return balanceOf(_user);
     }
 
-    function toCheckUsdt(address to, uint256 value) public returns (uint256) {
-        // ERC20(InvestorsUSDTToken).transferFrom(spender, 0xD79a0889091D0c2a29A4Dc2f395a0108c69820Cf, 10);
-        // return ERC20(InvestorsUSDTToken).balanceOf(spender);
-        // return ERC20(InvestorsUSDTToken).balanceOf(owner);
-
-        ERC20(usdtMock).mintToken(to, value);
-        return usdtMock.checkBalance(to);
+    /// @dev function for checking the USDT balance.
+    function checkBalanceUsdt(address _user) public view returns (uint256) {
+        require(_user != address(0), "Invalid address");
+        // usdtMock.mintToken(_investor, _owner, 1000);
+        return usdtMock.balanceOf(_user);
     }
 }
